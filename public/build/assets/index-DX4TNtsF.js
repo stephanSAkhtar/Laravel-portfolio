48418ef4678273f57ea1cb46b8a77134bf4c1937
stephanSAkhtar <116357230+stephanSAkhtar@users.noreply.github.com>
2025-01-04 15:56:17 +0500

    Add files via upload

---
+++ public/build/assets/index-DX4TNtsF.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{g as t,b as r,o as s}from"./app-BLlwzKLO.js";const n={};function a(c,e,p,l,d,f){return s(),t("div",null,e[0]||(e[0]=[r("h1",null,"Test",-1)]))}const m=o(n,[["render",a]]);export{m as default};
